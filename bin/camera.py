e82bc7227faf380802fc3a459d2c57f405225abc
Miriam Schlindwein <miriam.schlindwein@t-online.de>
2018-08-29 10:39:23 +0200

    fix tabs

--- bin/camera.py
+++ bin/camera.py
@@ -9,16 +9,16 @@ while(cap.isOpened()):
     # Capture frame-by-frame
     ret, frame = cap.read()
     if ret==True:
-    	frame = cv2.flip(frame, 0)
-    	
-    	out.write(frame)
-		
-		cv2.imshow('frame', frame)
-		
-		if cv2.waitKey(1) & 0xFF == ord('q'):
-			break
-	else:
-		break
+        frame = cv2.flip(frame, 0)
+
+        out.write(frame)
+
+        cv2.imshow('frame', frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
+        break
 
     # Our operations on the frame come here
     #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
