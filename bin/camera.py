146dca83c68a95eab78d7db2b3f7731aa360f2d4
roomuser <roomuser@roomie.uni-regensburg.de>
2018-08-29 05:41:48 -0500

    working webcam image stream

--- bin/camera.py
+++ bin/camera.py
@@ -1,9 +1,15 @@
 import numpy as np
 import cv2
+from ffpyplayer.player import MediaPlayer
 
-# source: https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_gui/py_video_display/py_video_display.html
+# sources: https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_gui/py_video_display/py_video_display.html
+# https://stackoverflow.com/questions/46864915/python-add-audio-to-video-opencv
 
 cap = cv2.VideoCapture(0)
+
+# Define the codec and create VideoWriter object
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('output.avi',fourcc, 20.0, (640,480))
 
 while(cap.isOpened()):
     # Capture frame-by-frame
@@ -17,6 +23,12 @@ while(cap.isOpened()):
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
+            
+        audio_frame, val = player.get_frame()
+        
+        if val != 'eof' and audio_frame is not None:
+            #audio
+            img, t = audio_frame
     else:
         break
 
